a18910a59c598dae601d01215b899157e200b4c4
buuzzy <buuzzy@163.com>
2025-08-11 09:22:32 +0800

    modify page.tsx

--- src/app/[locale]/(default)/page.tsx
+++ src/app/[locale]/(default)/page.tsx
@@ -1,24 +1,43 @@
+import Branding from '@/components/blocks/branding';
 import CTA from '@/components/blocks/cta';
+import FAQ from '@/components/blocks/faq';
+import Feature from '@/components/blocks/feature';
+import Feature1 from '@/components/blocks/feature1';
+import Feature2 from '@/components/blocks/feature2';
+import Feature3 from '@/components/blocks/feature3';
+import Hero from '@/components/blocks/hero';
+import Pricing from '@/components/blocks/pricing';
+import Showcase from '@/components/blocks/showcase';
+import Stats from '@/components/blocks/stats';
+import Testimonial from '@/components/blocks/testimonial';
+import { getLandingPage } from '@/services/page';
 import { useTranslations } from 'next-intl';
 
-export default function Page() {
-  const t = useTranslations('landing');
+export default async function LandingPage({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const page = await getLandingPage(locale);
 
-  const section = {
-    name: 'cta',
-    title: t('title'),
-    description: t('description'),
-    buttons: [
-      {
-        title: t('cta_text'),
-        url: '/trade',
-      },
-    ],
-  };
+  if (!page) {
+    return <div>Page not found</div>;
+  }
 
   return (
     <>
-      <CTA section={section} />
+      {page.hero && <Hero hero={page.hero} />}
+      {page.branding && <Branding section={page.branding} />}
+      {page.introduce && <Feature1 section={page.introduce} />}
+      {page.benefit && <Feature2 section={page.benefit} />}
+      {page.usage && <Feature3 section={page.usage} />}
+      {page.feature && <Feature section={page.feature} />}
+      {page.showcase && <Showcase section={page.showcase} />}
+      {page.stats && <Stats section={page.stats} />}
+      {page.pricing && <Pricing pricing={page.pricing} />}
+      {page.testimonial && <Testimonial section={page.testimonial} />}
+      {page.faq && <FAQ section={page.faq} />}
+      {page.cta && <CTA section={page.cta} />}
     </>
   );
 }
